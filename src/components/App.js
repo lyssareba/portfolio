a845ea8b748588aabbdd62e85830e6afb8b53d5a
Alyssa Robinson <lyssareba@gmail.com>
2020-05-05 12:13:22 -0600

    portfolio Init

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './Home';
+import DynamicPage from './DynamicPage';
+import NoMatch from './NoMatch';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/dynamic" component={DynamicPage} />
+        <Route component={Match} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  // Webpack configuration goes here
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[hash].js'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+
+      // First Rule
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+
+      // Second Rule
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localsConvention: 'camelCase',
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Robinson Portfolio',
+      // filename: 'public/index.html',
+      template: 'underscore-template-loader!./public/index.html',
+      favicon: 'public/favicon.ico',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1',
+      },
+    })
+  ],
+  devServer: {
+    host: 'localhost',
+    port: port,
+    historyApiFallback: true,
+    open: true
+  },
+};
