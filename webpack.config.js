c2fc99c323fb362e2475d107b0d479fe675d50e2
Alyssa Robinson <lyssareba@gmail.com>
2020-05-07 21:13:14 -0600

    R1 | bundle by vendor

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,10 @@ const port = process.env.PORT || 3000;
 module.exports = {
   // Webpack configuration goes here
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    vendor: ['semantic-ui-react'],
+    app: './src/index.js',
+  },
   output: {
     filename: '[name].[hash].js',
     publicPath: '/',
@@ -54,6 +57,24 @@ module.exports = {
         ]
       },
     ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css/,
+          chunks: 'all',
+          enforce: true,
+        },
+        vendor: {
+          chunks: 'initial',
+          test: 'vendor',
+          name: 'vendor',
+          enforce: true,
+        }
+      }
+    }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
